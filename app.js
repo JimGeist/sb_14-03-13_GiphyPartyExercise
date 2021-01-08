7a1ce767a1b5904ccbeced3f2681044d90c2fe3f
Jim Geist <info@jimgeist.com>
2021-01-08 13:58:59 -0500

    ver1.01: implemenation of class Giphy and creation of numerous methods to maintain Giphy information.

--- app.js
+++ app.js
@@ -10,6 +10,68 @@ const myGiphyIds = new Set();
 // create an array that will hold the Id and url for all the displayed Giphys.
 const myGiphys = [];
 
+class Giphy {
+
+    constructor() {
+
+        //this.giphys = [];
+        this.giphys = {};
+        this.giphyIds = new Set();
+        this.nbrGiphysAdded = 0;
+        //this.nbrGiphysOnPage = 0;
+
+    }
+
+
+    // saveGiphy(giphyId, giphyUrl) {
+    //     this.giphys[giphyId] = giphyUrl;
+    // }
+
+    wasGiphySaved(giphyId, giphyUrl) {
+
+        // returns true when giphyId and url were saved
+        if (this.giphyIds.has(giphyId)) {
+            return false;
+        } else {
+            this.giphyIds.add(giphyId);
+            this.giphys[giphyId] = giphyUrl;
+            this.nbrGiphysAdded++;
+            this.nbrGiphysOnPage++;
+
+            return true;
+        }
+
+    }
+
+    removeGiphy(giphyId) {
+        if (this.giphyIds.has(giphyId)) {
+            //this.nbrGiphysOnPage--;
+            this.giphyIds.delete(giphyId);
+            delete this.giphys[giphyId];
+        }
+    };
+
+    resetAddedCtr() {
+        this.nbrGiphysAdded = 0;
+    }
+
+    getAddedCtr() {
+        return this.nbrGiphysAdded;
+    }
+
+    getTotalGiphys() {
+        return this.giphyIds.size;
+    }
+
+    resetAll() {
+        this.giphys = {};
+        this.giphyIds.clear();
+        this.nbrGiphysAdded = 0;
+    }
+
+}
+
+const mg = new Giphy();
 
 function setMessage(inMsg, inMsgIsCritical) {
 
@@ -43,6 +105,17 @@ function displayGiphys(inGiphys) {
         buildAppendImg(inGiphys[idx]);
         // retain the id and url of the displayed image
         myGiphys.push(inGiphys[idx]);
+        //mg.saveGiphy(inGiphys[idx].id, inGiphys[idx].imgDownsizeMedUrl);
+    }
+
+    // were any giphys added?
+    if (mg.getAddedCtr() > 0) {
+        setMessage(`${mg.getAddedCtr()} GIPHYs were added to your page. `
+            + `There are ${mg.getTotalGiphys()} GIPHYs on your page. `
+            + "Click on a single GIPHY to remove it, click on 'Remove all GIPHYs' to remove all GIPHYs.", false);
+
+        //inputSearch.value = '';
+        mg.resetAddedCtr();
     }
 
 }
@@ -62,27 +135,50 @@ function selectGiphys(inData) {
             // randomly select 5 for display
             let ctr = 0;
             while (ctr < 5) {
+
+                // build a success message with number of giphys returned / displayed and 
+                // to click on individual giphys to remove.
+
                 idx = Math.floor(Math.random() * inData.length)
-                if (myGiphyIds.has(inData[idx].id) == false) {
-                    myGiphyIds.add(inData[idx].id);
+                if (mg.wasGiphySaved(inData[idx].id, inData[idx].images.downsized_medium.url)) {
                     outGiphys.push({
                         id: inData[idx].id,
                         imgDownsizeMedUrl: inData[idx].images.downsized_medium.url
                     });
                     ctr++;
                 }
+                // if (myGiphyIds.has(inData[idx].id) == false) {
+                //     myGiphyIds.add(inData[idx].id);
+                //     outGiphys.push({
+                //         id: inData[idx].id,
+                //         imgDownsizeMedUrl: inData[idx].images.downsized_medium.url
+                //     });
+                //     ctr++;
+                // }
+
+
             }
 
         } else {
             // use them all
             for (idx = 0; idx < inData.length; idx++) {
-                if (myGiphyIds.has(inData[idx].id) == false) {
-                    myGiphyIds.add(inData[idx].id);
+                // build a success message with number of giphys returned / displayed and 
+                // to click on individual giphys to remove.
+
+                if (mg.wasGiphySaved(inData[idx].id, inData[idx].images.downsized_medium.url)) {
                     outGiphys.push({
                         id: inData[idx].id,
                         imgDownsizeMedUrl: inData[idx].images.downsized_medium.url
                     });
                 }
+
+                // if (myGiphyIds.has(inData[idx].id) == false) {
+                //     myGiphyIds.add(inData[idx].id);
+                //     outGiphys.push({
+                //         id: inData[idx].id,
+                //         imgDownsizeMedUrl: inData[idx].images.downsized_medium.url
+                //     });
+                // }
             }
         }
     }
@@ -147,9 +243,11 @@ btnSearch.addEventListener("click", function (event) {
         // something was entered. Let's do it!
         getGiphyGiphs(inputSearch.value.trim());
 
+
+
     }
 
-    //inputSearch.value = '';
+
 
 })
 
@@ -163,28 +261,29 @@ btnRemove.addEventListener("click", function (event) {
     //  or a loop to run through myGiphyIds for the Ids
     $('img.img-giphy').remove();
 
-    myGiphyIds.clear();
+    mg.resetAll();
 
-    for (let ctr = myGiphys.length; ctr > 0; ctr--) {
-        myGiphys.pop();
-    }
+    // myGiphyIds.clear();
+
+    // for (let ctr = myGiphys.length; ctr > 0; ctr--) {
+    //     myGiphys.pop();
+    // }
 
 })
 
 
-// jquery to remove an image if selected
+// jquery to remove an image when selected
 $("#giphy-giphs").on("click", "img", function () {
 
     // remove the giphy from myGiphyIds set 
     const giphyId = $(this).attr("id");
-    myGiphyIds.delete(giphyId);
 
-    // // remove the giphy from myGiphys array.
-    // myGiphys = myGiphys.filter(function (value, array, idx) {
-    //     return value.id !== giphyId;
-    // })
+    mg.removeGiphy(giphyId);
+
+    setMessage(`One GIPHY was removed, ${mg.getTotalGiphys()} remain on your page. `, false);
 
     $(this).remove();
+
 })
 
 
